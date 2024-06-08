8003b4f30194a9cdba73e7ad5e4d0f9765f350b0
Shikha Bista <shikhabista33@gmail.com>
2024-06-08 08:31:42 +0545

    wip: file upload

---
+++ QB_Brain/Areas/IT/VIewModels/RepositoryCreateVm.cs
@@ -0,0 +1,9 @@
+﻿namespace IT_Web.Areas.IT.VIewModels;
+
+public class RepositoryCreateVm
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string Visibility { get; set; }
+    public string? RepoPath { get; set; }
+}
